442f51492c1ce97eb5566a7bb440c66ee509b8b3
Mukhil <mukhil.padmanabhan@raksan.in>
2018-02-11 12:20:24 +0530

    Added db controller

---
+++ dbActionHandler.js
@@ -0,0 +1,37 @@
+function handleDbEvents(collection, action, query, eventName, socket) {
+    switch(action){
+        case 'find':{
+            var temp2 = [];
+            collection.find(query).limit(100).toArray((err, res) =>{
+                if(err) throw err;
+                socket.emit(eventName, res);      
+            });
+            break;
+        }
+        case 'insert': {
+            var temp = [];
+            collection.insert(query, (err, data)=>{
+                temp.push(query)
+                var temp1 = temp.map(function(info){
+                info.date = new Date(info.date).toUTCString();
+                var diff = Date.parse(new Date()) - Date.parse(new Date(info.date));
+                if(diff >= 0) return info; else return;
+                });
+                socket.emit(eventName, temp1);
+                var x = +new Date(query.date)
+                var y =  Date.now();
+                var z = y - x;
+                console.log(z)
+                sendStatus({
+                    message: (z > 0)  ? "Message sent" : `Message scheduled to be sent on ${query.date}`,
+                    clear: true
+                });
+            });
+        }
+    }
+}
+
+
+module.exports = {
+   dbController :handleDbEvents
+};
--- index.js
+++ index.js
@@ -6,85 +6,55 @@ var io = require('socket.io')(http);
 var cron = require('node-cron');
 var Moment = require('moment');
 var result = [];
+var dbAction = require('./dbActionHandler.js')
 
-app.get('/', function(req, res){
+app.get('/', function(req, res){    
   res.sendFile(__dirname + '/index.html');
 });
 http.listen(3000, function(){
-
-mongo.connect('mongodb://127.0.0.1/mongochat', function(err, db){   // Connect to mongo
-    if(err) throw err;
-    client.on('connection', function(socket){
-        const myAwesomeDB = db.db('mongochat')
+    mongo.connect('mongodb://127.0.0.1/mongochat', function(err, db){   // Connect to mongo
+        if(err) throw err;
+        client.on('connection', function(socket){
+         const myAwesomeDB = db.db('mongochat')
          var chat = myAwesomeDB.collection('chats');
-        sendStatus = function(status) {
-            socket.emit('status', status);
+         sendStatus = function(status) {
+         socket.emit('status', status);
+        }
+        var query = {
+            "date":{"$lte": new Date()}
         }
         new cron.schedule('0,30 * * * * *', function () {
-        console.log('running a task every minute');
-        chat.find({"date":{"$lte": new Date().toUTCString()}}).limit(100).toArray(function(err, res){
-            if(err) throw err;
-            socket.emit('cleared');
-            socket.emit('output', res);              
+            console.log("CRON running....")
+            dbAction.dbController(chat, 'find' , query , 'output', socket);
+            socket.emit('cleared');   
         });
-    });
+        dbAction.dbController(chat, 'find' , query , 'output', socket);
+            
 
-        chat.find({"date":{"$lte": new Date().toUTCString()+(+0530)+' '+'(IST)'}}).limit(100).toArray(function(err, res){
-            if(err) throw err;
-            socket.emit('output', res);           
-        });
-
-        // Handle input events
+            // Handle input events
         socket.on('input', function(data){
             var name = data.name;
             var message = data.message;
-            var date = new Date(data.date);
-            if(name == '' || message == ''){  // Check for name and message
-                sendStatus('Please enter a name and message');
-            } else {
-                var temp = [];
-                chat.insert({name: name, message: message, date: date}, function(){ // Insert message
-                    
-                temp.push(data)
-                var x = temp.filter(function(info){
-                info.date = new Date(info.date).toUTCString();
-                var diff = Date.parse(new Date()) - Date.parse(new Date(info.date));
-                //console.log("diff", diff)
-                if(diff >= 0) return info;
-                });
-                client.emit('output', temp);
+            var date = data.date;
+            if(name == '' || message == '' || date == '')sendStatus('Please enter a name, date and message');
+            else {
+                var toInsert = {
+                    name: name, 
+                    message: message, 
+                    date: new Date(date).toUTCString()
+            }
+                dbAction.dbController(chat, 'insert' , toInsert , 'output', socket);
                 sendStatus({
-                    message: 'Message sent',
+                    message: `Message scheduled to be sent on ${toInsert.date}`,
                     clear: true
                 });
-            });
-        }
-    });
-                socket.on('clear', function(data){
-                chat.remove({}, function(){
+            }
+        });
+        socket.on('clear', function(data){
+            chat.remove({}, function(){
                 socket.emit('cleared');
-                });
             });
+        });
         });
     });
 });
-
-
-
-
-
-
-
-//new Moment(dateValue, 'DD-MM-YYYY').format('DD-MM-YYYY');
-
-        // Get chats from mongo collection
-//new Moment(vacationData.startDate).format('DD-MM-YYYY')
-
-//new Moment(vacationData.startDate + ' ' + vacationData.startTime, 'DD-MM-YYYY HH:mm:ss');
-
-        // var x = res.map(function(info){
-            //  info.date = new Date(info.date).toUTCString();
-            //  var diff = Date.parse(new Date()) - Date.parse(new Date(info.date));
-            //  console.log("diff", diff)
-            //  if(diff >= 0) return info;
-            // });
